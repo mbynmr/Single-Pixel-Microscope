0b18265ab32e3c09deb8c15ac704180c79ecd730
Labuser <mbynmr@nottingham.ac.uk>
2022-03-03 13:02:25 +0000

    cleaned up a lot

--- camera.py
+++ camera.py
@@ -10,22 +10,22 @@ from tqdm import tqdm
 class Camera:
     def __init__(self, resolution):
         self.resolution = resolution
-        pixels = np.asarray(self.resolution).prod()  # e.g. 128*128
+        pixels = np.asarray(self.resolution).prod()  # 32*32 = 1024
         self.DMD_resolution = [608, 684]
         self.factor = int(self.DMD_resolution[0] / self.resolution[0])  # todo floor, and pad vertically to stop rounding
         self.pad_width = int((self.DMD_resolution[1] * 2 -
                               np.kron(np.zeros(self.resolution), np.ones((self.factor, self.factor * 2))).shape[0]))
-        # 760
+        # pad_width = 760
 
         # Hadamard matrix
         self.hadamard_mat = hadamard(pixels)
         self.hadamard_all = np.zeros([pixels * 2, pixels])
         self.hadamard_all[0::2, ...] = (1 + self.hadamard_mat) / 2  # 1 & 0
         self.hadamard_all[1::2, ...] = (1 - self.hadamard_mat) / 2  # 0 & 1
+        # self.hadamard_all = 1 - self.hadamard_all  # todo check if 1 is toward the sample and 0 is away or not
 
         # set up fullscreen cv2 output (masks on the DMD)
-
-        self.display_time = 200 * 1e-3  # in seconds = 3 * (1 / 60)
+        self.display_time = 3 * (1 / 60)  # in seconds = 3 * (1 / 60)
         self.display_time_ms = int(self.display_time * 1e3)  # in milliseconds
         # self.display_time_ms = int(1000 / 60)  # in milliseconds
         self.window = set_up_mask_output()
@@ -34,6 +34,8 @@ class Camera:
         self.rm, self.multimeter = set_up_multimeter()
         # prepare the multimeter for a run of measurements
         self.reset_multimeter()
+        # set the measurement time
+        self.measurement_time = (self.display_time_ms * 3) / 1000  # in seconds, approximately 3 frames long
 
     def take_picture(self):
         measurements_p_and_m = self.measure()
@@ -54,7 +56,7 @@ class Camera:
             # convert the plus & minus hadamard matrixes into the correct images to be displayed on the DMD
             # turn 32x32 masks into 608x1216 by integer scaling
             mask_show = np.kron(mask.reshape(self.resolution), np.ones((self.factor, self.factor * 2)))
-            # pad the rectangular mask with zeros on both sides, so the on the DMD it appears as a roughly centred square
+            # pad the rectangular mask with zeros on both sides, so the on the DMD it appears as a centred square
             mask_show = np.pad(mask_show, ((0, 0), (self.pad_width - 400, 400)))  # 608x1976  # todo 400
             # the DMD rotates the image 90 degrees clockwise, so we need to do the opposite
             mask_show = np.rot90(mask_show, axes=(1, 0))  # 1976x608
@@ -63,34 +65,38 @@ class Camera:
             cv2.imshow(self.window, np.uint8(mask_show * 255))
             # show the window for the display time
             cv2.waitKey(self.display_time_ms)
+            # time.sleep(1/60)  # 16.666ms
+            time.sleep(17 * 1e-3)  # 17ms
 
             # the first <100ms that the mask is displayed is spent waiting
             # print((time.time() - start) % self.display_time)
-            try:
-                time.sleep(100e-3 - (time.time() - start) % self.display_time)  # todo 100e-3
-            except ValueError:
-                # if we miss this frame, wait until the next
-                print("Too slow! Waiting until the next frame...")
-                time.sleep(self.display_time + 100e-3 - (time.time() - start) % self.display_time)
+            # try:
+            #     time.sleep(100e-3 - (time.time() - start) % self.display_time)  # todo 100e-3
+            # except ValueError:
+            #     # if we miss this frame, wait until the next
+            #     print("Too slow! Waiting until the next frame...")
+            #     time.sleep(self.display_time + 100e-3 - (time.time() - start) % self.display_time)
 
             # the next ~400ms is spent taking measurements
             # tell the multimeter to wait for a trigger (which happens instantly since the trigger is set to immediate)
             self.multimeter.write('INITiate')
-            time.sleep(0.001)
+            time.sleep(self.measurement_time)  # todo measurement time
 
             # print(self.my_instrument.query('DATA: POINts?'))
             # if int(self.my_instrument.query('DATA: POINts?')) > 900:
-            if len(self.multimeter.query('FETCh?').split(',')) > 900:
-                print("resetting multimeter")
-                self.reset_multimeter()
+            # buffer = self.multimeter.query('FETCh?').split(';')[0].split(',')
+            # if len(buffer) > 950:  # todo 950? 1900?
+            #     print("resetting multimeter")
+            #     self.reset_multimeter()
             # time.sleep(self.display_time - (time.time() - start) % self.display_time)
-            for _ in range(5):
-                buffer = self.multimeter.query('FETCh?')
-                print(buffer)
-                print(buffer.split(';')[0].split(','))
-            print("once")
+            # for _ in range(5):
+            #     buffer = self.multimeter.query('FETCh?')
+            #     print(buffer)
+            #     print(buffer.split(';')[0].split(','))
+            # print("once")
             # Fetch the data from the instruments internal storage, then take the average of them
-            # measurements[i] = np.mean(np.array(self.my_instrument.query('FETCh?').split(','), dtype=float))
+            print(self.multimeter.query('FETCh?').split(';')[0].split(','))
+            measurements[i] = np.mean(np.array(self.multimeter.query('FETCh?').split(';')[0].split(','), dtype=float))
             # print(self.my_instrument.query('FETCh?'))
 
         return measurements
@@ -102,7 +108,7 @@ class Camera:
         self.multimeter.write('*RST')
 
         # Configure the instrument to measure DC Voltage, with a maximum range, and the minimum resolution
-        self.multimeter.write('CONFigure:VOLTage:DC 10, MIN')
+        self.multimeter.write('CONFigure:VOLTage:DC 10, MIN')  # todo 10V is too high, right?
 
         # set the number of PLC for each integration - i.e. measurement time
         self.multimeter.write('SENSe:VOLTage:DC:NPLCycles MINimum')
